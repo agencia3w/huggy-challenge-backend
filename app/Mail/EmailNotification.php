fcbbc2583f9ea3e8b61e7ecf7411cfdee6289f05
Paulinho <paulinho@agencia3w.com.br>
2022-01-02 13:04:30 -0300

    Email Notification

---
+++ app/Mail/EmailNotification.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class EmailNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $subject = $this->data['subject'];
+        $message = $this->data['message'];
+        $type = $this->data['type'];
+
+        return $this->view('email')
+            // ->from($address)
+            ->subject($subject)
+            ->with([
+                'content' => $message,
+                'subject' => $subject,
+                'type' => $type
+            ]);
+    }
+}
